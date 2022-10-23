e0667886fbd1362cb402d075ffebc70149cb322e
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-10-23 11:17:12 +0300

    Practice_task_2

--- practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_2/MyString.cpp
+++ practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_2/MyString.cpp
@@ -4,23 +4,23 @@
 //Метод копіювання рядка
 void MyString::copy(const char* str)
 {
-	delete[] m_string;
+	delete[] string;
 
-	m_length = strlen(str);
-	m_string = new char[m_length + 1];
+	length = strlen(str);
+	string = new char[length + 1];
 
 	int i = 0;
 	while (*(str + i) != '\0')
 	{
-		*(m_string + i) = *(str + i);
+		*(string + i) = *(str + i);
 		i++;
 	}
-	*(m_string + m_length) = '\0';
+	*(string + length) = '\0';
 }
 
 //Конструктор з параметрами
-MyString::MyString(const char* initString) :
-	m_length(strlen(initString))
+MyString::MyString(const char* initString):
+	length(strlen(initString))
 {
 	copy(initString);
 }
@@ -28,16 +28,16 @@ MyString::MyString(const char* initString) :
 //Деструктор
 MyString::~MyString()
 {
-	delete[] m_string;
+	delete[] string;
 }
 
 //Група "гетерів"
 char* MyString::getString() const
 {
-	return m_string;
+	return string;
 }
 
 int MyString::getLength() const
 {
-	return m_length;
+	return length;
 }
--- practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_2/MyString.h
+++ practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_2/MyString.h
@@ -8,14 +8,20 @@ using namespace std;
 class MyString
 {
 private:
-	char* m_string;
-	int m_length;
+	char* string;
+	int length;
 
 public:
+	//Конструктор з параметром
 	MyString(const char* initString);
+
+	//Деструктор
 	~MyString();
 
+	//Група "гетерів"
 	char* getString() const;
 	int getLength() const;
+
+	//Метод друку рядку
 	void copy(const char* str);
 };
