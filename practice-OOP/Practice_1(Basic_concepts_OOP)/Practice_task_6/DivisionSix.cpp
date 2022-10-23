a9148fad4b74a1937a6774ae9ee102dd48c07ee1
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-10-23 11:29:22 +0300

    Practice_task_6

--- practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_6/DivisionSix.cpp
+++ practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_6/DivisionSix.cpp
@@ -1,29 +1,30 @@
 ﻿#include "DivisionSix.h"
 
-//Конструктор за замовченням
+//Конструктор за замовчуванням
 DivisionSix::DivisionSix():
-	m_sum(0),m_leftIndex(0), m_rightIndex(0)
+	sum(0), leftIndex(0), rightIndex(0)
 {
 }
 
 //Сетер для правої межі
 void DivisionSix::setRightIndex(const int index)
 {
-	m_rightIndex = index;
+	this->rightIndex = index;
 }
 
 //Метод для підрахунку суми
 int DivisionSix::calculateSum()
 {
-    m_leftIndex = 0;
+    this->leftIndex = 0;
+    this->sum = 0;
 
-    while (m_leftIndex <= m_rightIndex)
+    while (leftIndex <= rightIndex)
     {
-        if (m_leftIndex % m_Number == 0)
+        if (leftIndex % Number == 0)
         {
-            m_sum += m_leftIndex;
+            sum += leftIndex;
         }
-        m_leftIndex++;
+        leftIndex++;
     }
-    return m_sum;
+    return sum;
 }
--- practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_6/DivisionSix.h
+++ practice-OOP/Practice_1(Basic_concepts_OOP)/Practice_task_6/DivisionSix.h
@@ -7,16 +7,18 @@ using namespace std;
 class DivisionSix
 {
 private:
-	int m_sum; //Сума чисел
-	int m_rightIndex; //Права межа
-	int m_leftIndex; //Ліва межа
-	const int m_Number = 6; //Число на яке ділимо
+	int sum; //Сума чисел
+	int rightIndex; //Права межа
+	int leftIndex; //Ліва межа
+	const int Number = 6; //Дільник
 
 public:
-	//Конструктор за замовченням
+	//Конструктор за замовчуванням
 	DivisionSix(); 
+
 	//Сетер для правої межі
 	void setRightIndex(const int index);
+
 	//Метод для підрахунку суми
 	int calculateSum();
 };
