2d2192768aef3ba6697c21a8334b3d75a2a1765b
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-10-30 16:01:13 +0200

    Practice_task_4

---
+++ practice-OOP/Practice_2(Methods_overloading)/Practice_task_4/BankAccount.cpp
@@ -0,0 +1,85 @@
+#include "BankAccount.h"
+
+//Конструктор з параметрами
+BankAccount::BankAccount(int balance , 
+	int depositInterest )
+{
+	this->balance = balance;
+	this->depositInterest = depositInterest;
+}
+
+//Конструктор копіювання
+BankAccount::BankAccount(const BankAccount& bankAcc)
+{
+	this->balance = bankAcc.balance;
+	this->depositInterest = bankAcc.depositInterest;
+}
+
+//Група "гетерів"
+int BankAccount::getBalance() const
+{
+	return this->balance;
+}
+
+int BankAccount::getDepositInterest() const
+{
+	return this->depositInterest;
+}
+
+//Група "сетерів"
+void BankAccount::setBalance(const int balance)
+{
+	this->balance = balance;
+}
+
+void BankAccount::setDepositInterest(const 
+							int deposInterest)
+{
+	this->depositInterest = deposInterest;
+}
+
+
+//Функція розрахунку доходу від вкладу
+int BankAccount::annualIncome() const
+{
+	return (balance * depositInterest / 100);
+}
+
+//Перевантаження оператора(=)
+BankAccount* BankAccount::operator=(const
+							BankAccount& bankAcc)
+{
+
+	this->balance = bankAcc.balance;
+	this->depositInterest = bankAcc.depositInterest;
+
+	return this;
+}
+
+//Перевантаження оператора(+)
+BankAccount BankAccount::operator+(const 
+							BankAccount& bankAcc)
+{
+	BankAccount tempAcc;
+	tempAcc.balance = this->balance + bankAcc.balance;
+	tempAcc.depositInterest = this->depositInterest +
+							  bankAcc.depositInterest;
+
+	return tempAcc;
+}
+
+//Перевантаження оператора("()")
+void BankAccount::operator()(const int  balance, 
+							const int depositInterest)
+{
+	if (balance * depositInterest / 100 >
+		this->balance * this->depositInterest / 100) {
+
+		this->balance = balance;
+		this->depositInterest = depositInterest;
+		cout << "Дані змінено!";
+	}
+	else {
+		cout << "Дані не змінено!";
+	}
+}
---
+++ practice-OOP/Practice_2(Methods_overloading)/Practice_task_4/Practice_task_4.cpp
@@ -0,0 +1,69 @@
+﻿/*
+
+    Реалізувати клас BankAccount. 
+    Поля класу:
+    - баланс;
+    - депозитний відсоток;
+    Методи класу: 
+    - конструктор;
+    - функція доступу до полів;
+    - розрахунок річного доходу від вкладу;
+    Перевантаження:
+    - оператор присвоєння;
+    - оператор суми, який складає баланс та відсоток;
+    - оператор(), який порівнює річний дохід введених
+    даних та наявих, якщо введені дані  мають більший
+    дохід перезаписуємо дані.
+
+*/
+
+
+#include "BankAccount.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    BankAccount bankAccFirst(400,4);
+    BankAccount bankAccSecond(500, 10);
+
+    cout << "Інформація про перший акаунт\n";
+    cout << "Баланс: " << 
+        bankAccFirst.getBalance() << endl;
+    cout << "Депозитний відсоток: " <<
+        bankAccFirst.getDepositInterest() << endl;
+    cout << "Річний прибуток: " <<
+        bankAccFirst.annualIncome() << endl;
+
+    cout << "\nІнформація про другий акаунт\n";
+    cout << "Баланс: " <<
+        bankAccSecond.getBalance() << endl;
+    cout << "Депозитний відсоток: " <<
+        bankAccSecond.getDepositInterest() << endl;
+    cout << "Річний прибуток: " <<
+        bankAccSecond.annualIncome() << endl;
+
+    BankAccount bankAccThird;
+    bankAccThird = bankAccFirst + bankAccSecond;
+
+    cout << "\nІнформація про третій акаунт\n";
+    cout << "Баланс: " <<
+        bankAccThird.getBalance() << endl;
+    cout << "Депозитний відсоток: " <<
+        bankAccThird.getDepositInterest() << endl;
+    cout << "Річний прибуток: " <<
+        bankAccThird.annualIncome() << endl;
+
+    cout << "\n--- Перевірка прибутку ---\n";
+    bankAccThird(1200, 12);
+
+    cout << "\n\nІнформація про третій акаунт\n";
+    cout << "Баланс: " <<
+        bankAccThird.getBalance() << endl;
+    cout << "Депозитний відсоток: " <<
+        bankAccThird.getDepositInterest() << endl;
+    cout << "Річний прибуток: " <<
+        bankAccThird.annualIncome() << endl;
+}
+
