d9accc761dff2e9ad763e3253932e08f122afc54
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-07 17:37:47 +0200

    practice_task_1

---
+++ practice-OOP/Practice_6(C++_style_files)/Practice_task_1/Car.cpp
@@ -0,0 +1,34 @@
+﻿#include "Car.h"
+
+//Конструктор з параметрами
+Car::Car(const string& brand, const double carPower, const int price)
+{
+	this->brand = brand;
+	this->enginePower = carPower;
+	this->price = price;
+}
+
+//Група "гетерів"
+string Car::getBrand() const
+{
+	return this->brand;
+}
+
+double Car::getEnginePower() const
+{
+	return this->enginePower;
+}
+
+int Car::getPrice() const
+{
+	return this->price;
+}
+
+//Перевантаження оператора(<<)
+ostream& operator<<(ostream& out, const Car& car)
+{
+	out << "Бренд: " << car.brand << " ,потужність: " << car.enginePower
+		<< " ,ціна: " << car.price;
+
+	return out;
+}
