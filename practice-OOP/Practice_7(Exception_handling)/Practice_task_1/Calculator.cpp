89826945cbfb6f8ebcf10e79633198c99605f5eb
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-09 17:58:32 +0200

    Practice_task_1

---
+++ practice-OOP/Practice_7(Exception_handling)/Practice_task_1/Calculator.cpp
@@ -0,0 +1,41 @@
+﻿#include "Calculator.h"
+
+//Функція суми
+int Calculator::sum(const int firstNumber, const int secondNumber)
+{
+    //Обробка виключення
+    if (firstNumber < 0 || secondNumber < 0)
+        throw MyException("Наявне від'ємне число!");
+
+    return firstNumber + secondNumber;
+}
+
+//Фукція множення
+int Calculator::multiplication(const int firstNumber, const int secondNumber)
+{
+    //Обробка виключення
+    if (firstNumber == 0 || secondNumber == 0)
+        throw MyException("Наявний нуль!");
+
+    return firstNumber * secondNumber;
+}
+
+//Функція різниці
+int Calculator::difference(const int firstNumber, const int secondNumber)
+{
+    //Обробка виключення
+    if ((firstNumber - secondNumber) < 0)
+        throw MyException("Різниця від'ємна!");
+
+    return firstNumber - secondNumber;
+}
+
+//Функція ділення
+int Calculator::division(const int firstNumber, const int secondNumber)
+{
+    //Обробка виключення
+    if (secondNumber == 0)
+        throw MyException("Дільник дорівнює 0!");
+
+    return firstNumber / secondNumber;
+}
---
+++ practice-OOP/Practice_7(Exception_handling)/Practice_task_1/MyException.cpp
@@ -0,0 +1,13 @@
+﻿#include "MyException.h"
+
+//Конструктор з параметрами
+MyException::MyException(const string& errorMessage)
+{
+    this->errorMessage = errorMessage;
+}
+
+//Функція, яка повертає повідомлення
+string MyException::what() const
+{
+    return this->errorMessage;
+}
