2af835b0bb4717d07e04b7b77b4e8e88ab0d70e0
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-15 18:33:23 +0200

    Practice_task_3

---
+++ practice-OOP/Practice_8(Template_classes)/Practice_task_3/Circle.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#define PI 3.1415
+
+template <typename Type>
+class Circle
+{
+protected:
+
+	//Координати
+	Type x;
+	Type y;
+
+	//Радіус
+	Type radius;
+
+public:
+
+	//Конструктор з параметрами
+	Circle(const Type x, const Type y,
+		const Type radius);
+
+	//Перевантаження конструктора з параметрами
+	Circle(const Type radius);
+
+	//Конструктор за замовчуванням
+	Circle();
+
+	//Фунція підрахунку довжини окружності
+	Type getCircumference() const;
+
+	//Функція підрахунку площі
+	Type getArea() const;
+
+	//Група "гетерів" для полів класу
+	Type getX() const;
+	Type getY() const;
+	Type getRarius() const;
+};
---
+++ practice-OOP/Practice_8(Template_classes)/Practice_task_3/Sphere.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Circle.h"
+
+template<typename Type>
+class Sphere : public Circle<Type>
+{
+protected:
+
+	//Додаткова координата
+	Type z;
+
+public:
+
+	//Конструктор з параметрами
+	Sphere(const Type x, const Type y, const Type z,
+		const Type radius);
+
+	//Перевантаження конструктора з параметрами
+	//Використаний параметр за замовчуванням
+	Sphere(const Type radius = 1);
+
+	//Функція підрахунку площі поверхну
+	Type getSurfaceArea() const;
+
+	//Функція підрахунку об'єму
+	Type getVolume() const;
+
+	//Гетер поля класу
+	Type getZ() const;
+};
