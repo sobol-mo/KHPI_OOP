aad0625067e9eea14f7778f1d6e88e61a07e3eb6
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-15 18:33:40 +0200

    Practice_task_4

---
+++ practice-OOP/Practice_8(Template_classes)/Practice_task_4/Account.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <iostream>
+using namespace std;
+
+class Account
+{
+private:
+
+	//Пошта
+	string mail;
+
+	//Пароль
+	string password;
+
+public:
+
+	//Конструктор з параметром
+	Account(const string& mail,
+	        const string& password);
+
+	//Функція зміни паролю
+	void setPassword(const string& newPassword);
+
+	//Функція друку інформації
+	void printInformation() const;
+};
