f9ba3efe83a3dfbbbf17b21db8a30078c3824915
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-20 16:53:43 +0200

    Practice_task_5

---
+++ practice-OOP/Practice_10(Functors)/Practice_task_5/Print.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+//Клас-функтор для
+//друку контейрнера map
+class Print
+{
+public:
+
+	//У перевантаженні оператора 
+	//використовується шаблон
+	template<typename Map>
+	void operator()(const Map& map) const;
+
+};
+
+//Метод реалізований у файлі .h
+//для того, щоб не оголошувати явно шаблони
+template<typename Map>
+inline void Print::operator()(const Map& map) const
+{
+	for (const auto& element : map) {
+		cout << "В наявності: " << element.first
+			<< " " << element.second << endl;
+	}
+}
