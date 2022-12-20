e699f4f630b1bb3bf806d7f7a2dcc136a9a2e61e
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-20 16:52:16 +0200

    Practice_task_1

---
+++ practice-OOP/Practice_10(Functors)/Practice_task_1/City.cpp
@@ -0,0 +1,34 @@
+﻿#include "City.h"
+
+//Конструктор з параметрами за замовчуванням
+City::City(const string& cityName, const int population)
+{
+	this->cityName = cityName;
+	this->population = population;
+}
+
+//Конструктор копіювання
+City::City(const City& city)
+{
+	this->cityName = city.cityName;
+	this->population = city.population;
+}
+
+//Функтор перевантажений для порівняння кількості населення
+bool City::operator()(const City& firstCity, const City& secondCity) const
+{
+	return firstCity.population > secondCity.population;
+}
+
+//Перевантаження оператора(<<)
+ostream& operator<<(ostream& out, const City& city)
+{
+	return out << "Назва: " << city.cityName
+		<< " | населення: " << city.population;
+}
+
+//Перевантаження оператора(<), порівнює назви міст
+bool operator<(const City& firstCity, const City& secondCity)
+{
+	return firstCity.cityName < secondCity.cityName;
+}
---
+++ practice-OOP/Practice_10(Functors)/Practice_task_1/City.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <iostream>
+using namespace std;
+
+class City
+{
+private:
+
+	//Назва міста
+	string cityName;
+
+	//Кількість населення
+	int population;
+
+public:
+
+	//Конструктор з параметрами за замовчуванням
+	City(const string& cityName = "", 
+	     const int population = 0);
+
+	//Конструктор копіювання
+	City(const City& city);
+
+	//Перевантаження оператора(<<)
+	friend ostream& operator<<(ostream& out, const City& city);
+
+	//Перевантаження оператора(<), порівнює назви міст
+	friend bool operator<(const City& firstCity, const City& secondCity);
+
+	//Функтор перевантажений для порівняння кількості населення
+	bool operator()(const City& firstCity, const City& secondCity) const;
+};
