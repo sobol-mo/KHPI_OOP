9a25f448122cb3362764c940680a5c8336962326
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-20 16:52:32 +0200

    Practice_task_2

---
+++ practice-OOP/Practice_10(Functors)/Practice_task_2/Sphere.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <iostream>
+using namespace std;
+
+class Sphere
+{
+private:
+
+	//Колір кулі
+	string color;
+
+	//Об'єм кулі
+	double volume;
+
+public:
+
+	//Конструктор з параметрами за замовчуваням
+	Sphere(const string& color = "",
+	       const double volume = 0);
+
+	//Конструктор копіювання
+	Sphere(const Sphere& sphere);
+
+	//Перевантаження оператора(<)
+	friend bool operator<(const Sphere& firstSphere, 
+	                      const Sphere& secondSphere);
+
+	//Функтор, порівнює об'єм куль
+	bool operator()(const Sphere& firstSphere,
+	                const Sphere& secondSphere) const;
+
+	//Перевантаження оператора(<<)
+	friend ostream& operator<<(ostream& out, const Sphere& sphere);
+};
+
