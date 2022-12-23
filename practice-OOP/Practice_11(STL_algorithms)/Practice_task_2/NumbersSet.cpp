593d3383863a8a67a74f42c5ffafcb63b68d7188
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2022-12-23 17:22:35 +0200

    Practice_task_2

---
+++ practice-OOP/Practice_11(STL_algorithms)/Practice_task_2/NumbersSet.cpp
@@ -0,0 +1,104 @@
+﻿#include "NumbersSet.h"
+
+//Функція розрахунку квадрата числа
+int square(const int number)
+{
+	return number * number;
+}
+
+//Конструктор з параметром
+NumbersSet::NumbersSet(const string& name)
+{
+	this->name = name;
+}
+
+//Функція додавання числа
+void NumbersSet::addNumber(const int number)
+{
+	numbersSet.push_back(number);
+
+	//Після додавання сортуємо list
+	numbersSet.sort();
+}
+
+//Функція зміни всіх чисел на їх квадрати
+void NumbersSet::changeToSquareNumber()
+{
+	if (numbersSet.empty())
+		throw exception("Множина чисел порожня!");
+
+	transform(numbersSet.begin(), numbersSet.end(), 
+		numbersSet.begin(), square);
+}
+
+//Функція об'єднання множин
+void NumbersSet::mergeSet(NumbersSet& set)
+{
+	numbersSet.merge(set.numbersSet);
+}
+
+//Функція видалення повторюваних чисел
+void NumbersSet::removeRepetitions()
+{
+	if (numbersSet.empty())
+		throw exception("Множина чисел порожня!");
+
+	//За допомогою функції unique видаляємо всі числа,
+	//які повторюються повертаючи ітератор на нову послідовність
+	auto it = unique(numbersSet.begin(), numbersSet.end());
+
+	//За допомогою методу resize і функції distance
+	//змінюємо фактичний розмір контейнера list
+	numbersSet.resize(distance(numbersSet.begin(), it));
+}
+
+//Функція знаходження перетину множин
+list<int> NumbersSet::getIntersection(const NumbersSet& set) const
+{
+	if (numbersSet.empty())
+		throw exception("Перша множина чисел порожня!");
+
+	if (set.numbersSet.empty())
+		throw exception("Друга множина чисел порожня!");
+
+	list<int> intersection;
+
+	//Пошук усіх повторів чисел у двох множинах 
+	set_intersection(numbersSet.begin(), numbersSet.end(),
+		set.numbersSet.begin(), set.numbersSet.end(),
+
+		//back_inserter потрібен для додавання
+		//знайденого числа у кінець intersection
+		back_inserter(intersection));
+
+	return intersection;
+}
+
+//Функція знаходження першої пари чисел, що розрізняється
+mismatch_pair NumbersSet::firstMismatch(const NumbersSet& set) const
+{
+	if (numbersSet.empty())
+		throw exception("Перша множина чисел порожня!");
+
+	if (set.numbersSet.empty())
+		throw exception("Друга множина чисел порожня!");
+
+	mismatch_pair pair = mismatch(numbersSet.begin(), numbersSet.end(),
+		set.numbersSet.begin(), set.numbersSet.end());
+
+	return pair;
+}
+
+//Функція друку множини
+void NumbersSet::print() const
+{
+	if (numbersSet.empty())
+		throw exception("Множина чисел порожня!");
+
+	cout << name << " = { ";
+
+	for (const auto& elem : numbersSet)
+		cout << elem << " ";
+	
+	cout << "}" << endl;
+}
---
+++ practice-OOP/Practice_11(STL_algorithms)/Practice_task_2/Practice_task_2.cpp
@@ -0,0 +1,92 @@
+﻿/*
+
+    Створити клас NumbersSet, з полями класу:
+    - назва множини;
+    - множина чисел, використовувати list(числа можуть повторюватися)
+    з типом даних int.
+    Методи класу:
+    - конструктор з параметром(назва);
+    - функція додавання числа(після додававання
+    відразу сортувати список);
+    - функція зміни всіх чисел на їх квадрати(функція transform);
+    - функція об'єднання множин;
+    - функція видалення повторюваних чисел(функція unique);
+    - функція знаходження перетин множин(функція set_intersection);
+    - функція знаходження першої пари чисел,
+    що розрізняються;
+    - функція друку множини.
+
+    Додатково реалізувати функцію розрахунку квадрату
+    числа. Для реалізації методів класу не використовувати
+    цикли і умовні оператори(крім функції друку).
+
+*/
+
+#include <Windows.h>
+#include "NumbersSet.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    NumbersSet firstSet("A");
+    NumbersSet secondSet("B");
+
+    try
+    {
+
+        //Заповнення множин
+        //з сортуванням
+        firstSet.addNumber(7);
+        firstSet.addNumber(7);
+        firstSet.addNumber(4);
+        firstSet.addNumber(9);
+        firstSet.addNumber(9);
+
+        secondSet.addNumber(5);
+        secondSet.addNumber(6);
+        secondSet.addNumber(4);
+
+        cout << " --- Друк множин ---\n";
+        firstSet.print();
+        secondSet.print();
+
+        //Пошук перетину множин
+        list<int> intersection;
+        intersection = firstSet.getIntersection(secondSet);
+
+        cout << "\n--- Перетин множин ---\n"
+            << "I = { ";
+        for (const auto& elem : intersection)
+            cout << elem << " ";
+        cout << "}" << endl;
+
+        //Пошук перешої пари, що не збігається
+        mismatch_pair mismatchPair = firstSet.firstMismatch(secondSet);
+        
+        cout << "\nПара чисел: " << *mismatchPair.first << ", "
+            << *mismatchPair.second << endl;
+        
+        //Всі числа зводимо у квадрат
+        secondSet.changeToSquareNumber();
+
+        //Видаляємо всі повтори чисел
+        firstSet.removeRepetitions();
+
+        cout << "\n--- Друк оновлених множин ---\n";
+        firstSet.print();
+        secondSet.print();
+
+        //Об'єднання множин
+        firstSet.mergeSet(secondSet);
+
+        cout << "\n--- Друк множин після об'єднання ---\n";
+        firstSet.print();
+        secondSet.print();
+    }
+    catch (const exception& exception)
+    {
+        cout << exception.what() << endl;
+    }
+}
