b6565a361c9cdba564be533b49fcb08ae2726c9b
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-04 16:17:11 +0200

    Practice_task_4

---
+++ practice-OOP/Practice_13(Assert_Testing)/Practice_task_4/Time.cpp
@@ -0,0 +1,63 @@
+﻿#include "Time.h"
+
+//Конструктор з параметром за замовчуванням
+Time::Time(const int hours, const int minutes, const int seconds)
+{
+
+	//За допомогою assert перевіряємо
+	//коректність даних
+	assert(hours <= 23 && hours >= 0);
+	assert(hours <= 59 && hours >= 0);
+	assert(seconds <= 59 && seconds >= 0);
+
+	this->hours = hours;
+	this->minutes = minutes;
+	this->seconds = seconds;
+}
+
+//Група сетерів класу
+Time& Time::setHours(const int hours)
+{
+	assert(hours <= 23 && hours >= 0);
+
+	this->hours = hours;
+
+	//Повертаємо поточний об'єкт
+	//для ланцюжка викликів методів
+	return *this;
+}
+
+
+Time& Time::setMinutes(const int minutes)
+{
+	assert(hours <= 59 && hours >= 0);
+
+	this->minutes = minutes;
+
+	return *this;
+}
+
+Time& Time::setSeconds(const int seconds)
+{
+	assert(seconds <= 59 && seconds >= 0);
+
+	this->seconds = seconds;
+
+	return *this;
+}
+
+//Перевантаження оператора(==) для порівняння часу
+bool Time::operator==(const Time& time) const
+{
+	return hours == time.hours &&
+	       minutes == time.minutes &&
+	       seconds == time.seconds;
+}
+
+//Перевантаження оператора(<<)
+ostream& operator<<(ostream& out, const Time& time)
+{
+	return out << time.hours   << ":"
+	           << time.minutes << ":"
+	           << time.seconds;
+}
---
+++ practice-OOP/Practice_13(Assert_Testing)/Practice_task_4/Time.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+class Time
+{
+private:
+
+	//Години
+	int hours;
+
+	//Хвилини
+	int minutes;
+
+	//Секунди
+	int seconds;
+
+public:
+
+	//Конструктор з параметром за замовчуванням
+	Time(const int hours = 0, 
+	     const int minutes = 0, 
+	     const int seconds = 0);
+
+	//Група сетерів класу
+	Time& setHours(const int hours);
+	Time& setMinutes(const int minutes);
+	Time& setSeconds(const int seconds);
+
+	//Перевантаження оператора(<<)
+	friend ostream& operator<<(ostream& out, const Time& time);
+
+	//Перевантаження оператора(==) для порівняння часу
+	bool operator==(const Time& time) const;
+};
