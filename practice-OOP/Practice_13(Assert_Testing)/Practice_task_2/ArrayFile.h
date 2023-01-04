755f171793f7a45c77308ae0d046ea5c90f73e60
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-04 16:16:31 +0200

    Practice_task_2

---
+++ practice-OOP/Practice_13(Assert_Testing)/Practice_task_2/ArrayFile.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <assert.h>
+using namespace std;
+
+//Функція тестування вектора
+void testingVector(const vector<int> vectorInt);
+
+class ArrayFile
+{
+private:
+
+	//Шлях до файлу
+	string path;
+
+public:
+
+	//Конструктор з параметром
+	ArrayFile(const string& path);
+
+	//Сетер для поля 
+	void setPath(const string& path);
+
+	//Функція зчитування масиву з файлу
+	vector<int> getArray() const;
+
+	//Функція заміни масива у файлі
+	void setArray(const vector<int> vectorInt) const;
+};
