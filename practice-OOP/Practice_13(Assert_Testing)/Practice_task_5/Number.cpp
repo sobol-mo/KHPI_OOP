a730f576a20e0c91d305152a40ee3a44cb8afe85
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-04 16:17:31 +0200

    Practice_task_5

---
+++ practice-OOP/Practice_13(Assert_Testing)/Practice_task_5/Number.cpp
@@ -0,0 +1,68 @@
+﻿#include "Number.h"
+
+//Конструктор з параметром
+Number::Number(const int value)
+{
+	this->value = value;
+}
+
+//Конструктор копіювання
+Number::Number(const Number& number)
+{
+	this->value = number.value;
+}
+
+//Сетер поля класу
+void Number::setValue(const int value)
+{
+	this->value = value;
+}
+
+int Number::getValue() const
+{
+	return this->value;
+}
+
+//Перевантаження оператора присвоювання
+Number& Number::operator=(const Number& number)
+{
+	this->value = number.value;
+
+	return *this;
+}
+
+//Функція розрахунку факторіалу числа
+int Number::getFactorial() const
+{
+	int result = 1;
+	int n = this->value;
+
+	while (n > 1)
+	{
+		assert(result <= INT_MAX / n);
+		result *= n;
+		n--;
+	}
+
+	return result;
+}
+
+//Группа перевантажень арифметичних операторів
+Number operator*(const Number& number, const int value)
+{
+	assert(value != 0 && value != 1);
+
+	return Number(number.value * value);
+}
+
+Number operator+(const Number& number, const int value)
+{
+	return Number(number.value + value);
+}
+
+Number operator/(const Number& number, const int value)
+{
+	assert(value != 0);
+
+	return Number(number.value / value);
+}
