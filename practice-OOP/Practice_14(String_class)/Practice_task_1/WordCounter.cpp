fe45144701948fc13a394eed8f8c466fdd90039f
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-08 17:49:26 +0200

    Practice_task_1

---
+++ practice-OOP/Practice_14(String_class)/Practice_task_1/WordCounter.cpp
@@ -0,0 +1,39 @@
+﻿#include "WordCounter.h"
+
+//Функція порівняння поточного слова з шуканим
+bool WordCounter::equal(const string& curWord, const string& word) const
+{
+
+    //Розділові знаки
+    char symbols[] = { ',', '.', '?', '!' };
+
+    //Якщо слова рівні, повертаємо true
+    if (curWord == word) return true;
+    
+    for (int i = 0; i < sizeof(symbols); i++)
+        //Додаємо до шуканого слова розділові знаки
+        //Якщо є збіг повертаємо true
+        if (curWord == word + symbols[i]) return true;
+
+    //Якщо слово не знайшли повертаємо false
+    return false;
+}
+
+int WordCounter::getCount(const string& path, const string& word) const
+{
+    ifstream fin(path);
+    if (!fin.is_open())
+        throw exception("Помилка відкриття файлу!");
+
+    string curWord; //Поточне слово з файлу
+    int count = 0; //Кількість слів
+
+    while (!fin.eof())
+    {
+        fin >> curWord;
+        if (this->equal(curWord, word)) count++;
+    }
+
+    fin.close();
+    return count;
+}
