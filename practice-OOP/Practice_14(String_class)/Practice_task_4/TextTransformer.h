b107ace9914052343b8b4afb5161c6679c82b333
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-08 17:50:42 +0200

    Practice_task_4

---
+++ practice-OOP/Practice_14(String_class)/Practice_task_4/TextTransformer.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class TextTransformer
+{
+private:
+
+	//Символ пошуку
+	char searchSymbol;
+
+	//Функція для зміни слова
+	void transformWord(string& word) const;
+
+public:
+
+	//Конструктор з параметром
+	TextTransformer(const char searchSymbol);
+
+	//Сетер поля класу
+	void setSymbol(const char searchSymbol);
+
+	//Перевантаження оператора круглих дужок
+	void operator()(const string& inputFilePath,
+		const string& outputFilePath) const;
+};
