f70b4dd118d126c55a879a682b4fd73d70776082
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-08 17:50:06 +0200

    Practice_task_2

---
+++ practice-OOP/Practice_14(String_class)/Practice_task_2/Practice_task_2.cpp
@@ -0,0 +1,30 @@
+﻿/*
+
+    Створити клас-функтор StringFilter,
+	який шукає символи, які зустрічаються
+	лише раз у тексті. Знайдені символи 
+	записати у файл в зворотньому порядку.
+
+*/
+
+#include <Windows.h>
+#include "StringFilter.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    StringFilter filter;
+    string text = "fffgdff213123hp";
+    const string path = "resultText.txt";
+
+	try
+	{
+		filter(path, text);
+	}
+	catch (const exception& exception)
+	{
+		cout << exception.what() << endl;
+	}
+}
---
+++ practice-OOP/Practice_14(String_class)/Practice_task_2/StringFilter.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//Клас-функтор
+class StringFilter
+{
+public:
+
+	//Перевантаження оператора круглих дужок 
+	void operator()(const string& path, const string& text) const;
+};
