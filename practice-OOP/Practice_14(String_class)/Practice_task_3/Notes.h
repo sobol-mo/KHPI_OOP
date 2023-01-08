6e512c8e4a851b314cb5bbc39edab2a0f233b56f
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-01-08 17:50:26 +0200

    Practice_task_3

---
+++ practice-OOP/Practice_14(String_class)/Practice_task_3/Notes.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+class Notes
+{
+private:
+
+	//Пароль
+	string password;
+
+	//Прапор входу до облікового запису
+	//1 - користувач увійшов у систему
+	//0 - користувач не увійшов
+	bool enterFlag;
+
+	//нотатки
+	map<int, string> notes;
+
+public:
+
+	//Конструктор з параметрами
+	Notes(const string& password);
+
+	//Функція входу до облікового запису
+	void login(const string& password);
+
+	//Функція виходу з облікового запису
+	void exit();
+
+	//Функція зміни паролю
+	void setPassword(const string& newPassword);
+
+	//Функція додавання нотатки
+	void addNote(const string& note);
+
+	//Функція заміни тексту в нотатці, аргументи:
+	// - номер нотатки
+	// - новий текст
+	// - початкова позиція для заміни
+	// - кількість змінюваних символів
+	void changeNote(const int index, const string& text,
+		const int deletePos, const int count);
+
+	//Друк нотаток
+	void printNotes() const;
+};
